170566c39c2523d729b80a73afa2300b080de8ee
Hassan <hassan.farahat@su.edu.eg>
2025-10-18 23:11:07 +0300

    second commit

--- Clac/Program.cs
+++ Clac/Program.cs
@@ -2,11 +2,22 @@
 {
     internal class Program
     {
-
+        public static int sum(int x,int y)
+        {
+            return x + y;
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter first number");
+
+            int x=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+
+            int y=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(sum(x,y));
         }
     }
 }
